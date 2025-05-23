dff07c96589ffc3adbb631570ba9434b3e6a4155
Bawa Ernest Kwame <bawaernest926@gmail.com>
2025-05-23 04:32:12 +0000

    chart section

---
+++ src/Components/Title.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Title = ({title}) => {
+  return (
+    <div className='text-2xl 2xl:text-3xl font-semibold text-gray-600 dark:text-gray-500'>
+        {title}</div>
+  )
+}
+
+export default Title;
